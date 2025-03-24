e3b50172d5c9577d465198dd03d3f76015ecfb57
Mahendra <root@lms.devstack.edx>
2025-03-24 16:15:24 +0000

    Fix certificate preview issue

--- lms/djangoapps/certificates/views/webview.py
+++ lms/djangoapps/certificates/views/webview.py
@@ -121,7 +121,11 @@ def _update_certificate_context(context, course, course_overview, user_certifica
     else:
         date = display_date_for_certificate(course, user_certificate)
     # Translators:  The format of the date includes the full name of the month
-    context['certificate_date_issued'] = strftime_localized(date, settings.CERTIFICATE_DATE_FORMAT)
+    # Added by Developer
+    if date:
+        context['certificate_date_issued'] = strftime_localized(date, settings.CERTIFICATE_DATE_FORMAT)
+    else:
+        context['certificate_date_issued'] = ""
 
     # Translators:  This text represents the verification of the certificate
     context['document_meta_description'] = _('This is a valid {platform_name} certificate for {user_name}, '
